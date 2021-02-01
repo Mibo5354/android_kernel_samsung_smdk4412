SmartPack/SmartPack-Kernel-Project_kltexxx

Boeffla sound: Simplified headphone and speaker gain sysfs These changes are currently required for the easy integration of this driver with SmartPack-Kernel Manager (and possibly official KA later) according to [this](https://github.com/SmartPack/SmartPack-Kernel-Manager/commit/247361059b9cd751c94b91dddf226da2ad41c0a1) commit.
Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

sound/soc/codecs/boeffla_sound.c
@@ -196,7 +196,7 @@ static ssize_t boeffla_sound_store(struct device *dev, struct device_attribute *
 static ssize_t headphone_volume_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	// print current values
-	return sprintf(buf, "Headphone volume:\nLeft: %d\nRight: %d\n", 
+	return sprintf(buf, "%d %d\n", 
 		headphone_volume_l, headphone_volume_r);
 }
 
@@ -243,7 +243,7 @@ static ssize_t headphone_volume_store(struct device *dev, struct device_attribut
 
 	// print debug info
 	if (debug)
-		printk("Boeffla-sound: headphone volume L=%d R=%d\n", headphone_volume_l, headphone_volume_r);
+		printk("%d %d\n", headphone_volume_l, headphone_volume_r);
 
 	return count;
 }
@@ -254,7 +254,7 @@ static ssize_t headphone_volume_store(struct device *dev, struct device_attribut
 static ssize_t speaker_volume_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	// print current values
-	return sprintf(buf, "Speaker volume:\nLeft: %d\nRight: %d\n", 
+	return sprintf(buf, "%d %d\n", 
 		speaker_volume_l, speaker_volume_r);
 }
 
@@ -301,7 +301,7 @@ static ssize_t speaker_volume_store(struct device *dev, struct device_attribute 
 
 	// print debug info
 	if (debug)
-		printk("Boeffla-sound: speaker volume L=%d R=%d\n", speaker_volume_l, speaker_volume_r);
+		printk("%d %d\n", speaker_volume_l, speaker_volume_r);
 
 	return count;
 }

Mibo5354/android_kernel_samsung_smdk4412

sound/soc/codecs/boeffla_sound.c
@@ -1453,7 +1453,7 @@ static ssize_t boeffla_sound_store(struct device *dev, struct device_attribute *
 static ssize_t headphone_volume_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	// print current values
-	return sprintf(buf, "Headphone volume:\nLeft: %d\nRight: %d\n", headphone_l, headphone_r);
+	return sprintf(buf, "%d %d\n", headphone_l, headphone_r);
 }
 
 
@@ -1496,7 +1496,7 @@ static ssize_t headphone_volume_store(struct device *dev, struct device_attribut
 
 	// print debug info
 	if (debug(DEBUG_NORMAL))
-		printk("Boeffla-sound: headphone volume L=%d R=%d\n", headphone_l, headphone_r);
+		printk("%d %d\n", headphone_l, headphone_r);
 
 	return count;
 }
@@ -1507,7 +1507,7 @@ static ssize_t headphone_volume_store(struct device *dev, struct device_attribut
 static ssize_t speaker_volume_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
 	// print current values
-	return sprintf(buf, "Speaker volume:\nLeft: %d\nRight: %d\n", speaker_l, speaker_r);
+	return sprintf(buf, "%d %d\n", speaker_l, speaker_r);
 
 }
 
@@ -1551,7 +1551,7 @@ static ssize_t speaker_volume_store(struct device *dev, struct device_attribute 
 
 	// print debug info
 	if (debug(DEBUG_NORMAL))
-		printk("Boeffla-sound: speaker volume L=%d R=%d\n", speaker_l, speaker_r);
+		printk("%d %d\n", speaker_l, speaker_r);
 
 	return count;
 }